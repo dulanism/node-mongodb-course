1db1fab66d8eb10f3cf78c4c5641c4810fddebf5
Dulani Wallace <dulani@dulani.com>
2020-09-23 01:12:47 -0400

    Fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# node-mongodb-course
-Place to store my lessons from a Nodejs-Mongo Course
---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const fs = require('fs')
+
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const homePage = fs.readFileSync('index.html')
+
+const server = http.createServer((request, response) => {
+
+    console.log(request.url)
+
+    if (request.url === '/about') {
+        
+        return response.end(aboutPage)
+   
+    }   else if (request.url === '/contact') {
+
+        return response.end(contactPage)
+    
+    }   else if (request.url === '/') {
+
+        return response.end(homePage)
+
+    }   else {
+
+ 
+
+        response.writeHead(404)
+
+    
+        response.end('THE PAGE WAS NOT FOUND.')
+    
+    }
+})
+
+server.listen(3000)
